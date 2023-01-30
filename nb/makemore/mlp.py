11210aef833cb3814242988b5c0159eb35760bf4
Shumayl <83513359+mshumayl@users.noreply.github.com>
2023-01-30 23:47:18 +0800

    MLP validation

--- nb/makemore/mlp.py
+++ nb/makemore/mlp.py
@@ -19,9 +19,7 @@ stoi['.'] = 0
 itos = {i:s for s, i in stoi.items()}
 
 # %%
-block_size = 3 # context size
-
-def build_dataset(words):
+def build_dataset(words, block_size: int = 3):
     X, Y = [], []
     
     for w in words:
@@ -41,7 +39,6 @@ def build_dataset(words):
     
     return X, Y
 
-# %%
 
 def split_dataset():
     random.seed(SEED)
@@ -57,26 +54,24 @@ def split_dataset():
     return xtr, ytr, xdev, ydev, xts, yts
 
     
-# %%
-def init_model():
+def init_model(hl_size: int = 200, lookup_size: int = 30):
     g = torch.Generator().manual_seed(SEED)
     C = torch.randn((27, 10), generator=g, requires_grad=True)
     
-    W1 = torch.randn((30, 200), generator=g, requires_grad=True)  
-    b1 = torch.randn(200, generator=g, requires_grad=True)        
+    W1 = torch.randn((lookup_size, hl_size), generator=g, requires_grad=True)  
+    b1 = torch.randn(hl_size, generator=g, requires_grad=True)        
     
-    W2 = torch.randn((200, 27), generator=g, requires_grad=True)  
+    W2 = torch.randn((hl_size, 27), generator=g, requires_grad=True)  
     b2 = torch.randn(27, generator=g, requires_grad=True)
     
-    parameters = [C, W1, b1, W2, b2]
+    model_params = [C, W1, b1, W2, b2]
     
-    return parameters
+    return model_params
 
-# %%
-def train_model(data: tuple, parameters: list, steps: int = 1, batch_size: int = 32):
+
+def train_model(data: tuple, model_params: list, steps: int = 1, batch_size: int = 32):
     x, y = data
-    C, W1, b1, W2, b2 = parameters
-    
+    C, W1, b1, W2, b2 = model_params
     
     for i in range(steps):
         # get random batch
@@ -89,13 +84,13 @@ def train_model(data: tuple, parameters: list, steps: int = 1, batch_size: int =
         loss = F.cross_entropy(logits, y[ix])
         
         # backward pass
-        for p in parameters:
+        for p in model_params:
             p.grad = None
         loss.backward()
         
         # update
         lr = 0.1 if i <100000 else 0.01 # decay
-        for p in parameters:
+        for p in model_params:
             p.data += -lr * p.grad
             
         # learning stats
@@ -104,7 +99,47 @@ def train_model(data: tuple, parameters: list, steps: int = 1, batch_size: int =
         
     print(f"{loss=}")
     plt.plot(stepi, lossi)
+
+
+def generate_samples(model_params: list, num_samples: int = 10, block_size: int = 3):
+    g = torch.Generator().manual_seed(SEED)
+    C, W1, b1, W2, b2 = model_params
+    
+    for _ in range(num_samples):
+        out = []
+        context = [0] * block_size
+        
+        while True:
+            emb = C[torch.tensor([context])] # [1, block_size, d]
+            h = torch.tanh(emb.view(1, -1) @ W1 + b1)
+            logits = h @ W2 + b2
+            probs = F.softmax(logits, dim=1)
             
+            ix = torch.multinomial(probs, num_samples=1, generator=g).item()
+            
+            context = context[1:] + [ix]
+            
+            out.append(ix)
+            
+            if ix==0:
+                break
+            
+        print(''.join(itos[i] for i in out))
+            
+
+def validate_model(model_params: list, data: tuple):
+    
+    C, W1, b1, W2, b2 = model_params
+    x, y = data
+
+    emb = C[x]
+    h = torch.tanh(emb.view(-1, 30) @ W1 + b1)
+    logits = h @ W2 + b2
+    loss = F.cross_entropy(logits, y)
+    
+    print(f"{loss=}")
+        
+
 # %%
 build_dataset(words)
 xtr, ytr, xdev, ydev, xts, yts = split_dataset()
@@ -118,8 +153,19 @@ stepi = []
 #%%
 train_model(
     data=(xtr, ytr),
-    parameters=model, 
+    model_params=model, 
     steps=100000,
-    batch_size=32
+    batch_size=128
     )
+# %%
+generate_samples(model)
+# %%
+datatr = (xtr, ytr)
+datadev = (xdev, ydev)
+datats = (xts, yts)
+
+validate_model(model, datatr)
+validate_model(model, datadev)
+# validate_model(model, datats)
+
 # %%
