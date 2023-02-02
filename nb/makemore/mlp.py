b0b8e5cc0a139401ee62d2daa72d723f684848d2
Shumayl <83513359+mshumayl@users.noreply.github.com>
2023-02-03 00:50:22 +0800

    BatchNorm for inference and evaluation

--- nb/makemore/mlp.py
+++ nb/makemore/mlp.py
@@ -10,7 +10,7 @@ with open(r"../../data/names.txt") as f:
     words = f.read().lower().splitlines()
 
 
-SEED = 12345
+SEED = 987654321
 # %%
 chars = sorted(list(set(''.join(words))))
 
@@ -64,19 +64,31 @@ def init_model(hl_size: int = 200, lookup_size: int = 10, context_length: int = 
     W2 = torch.randn((hl_size, 27), generator=g) * 0.01 # so that first step is not too far away
     b2 = torch.randn(27, generator=g) * 0.0 # zero bias on initialization
     
-    batchnorm_gain = torch.ones((1, hl_size)) # BatchNorm normalizes preactivation values - reduces extremities
-    batchnorm_bias = torch.ones((1, hl_size))
+    bn_gain = torch.ones((1, hl_size)) # BatchNorm normalizes preactivation values - reduces extremities
+    bn_bias = torch.zeros((1, hl_size))
+    bn_mean_running = torch.zeros((1, hl_size))
+    bn_std_running = torch.ones((1, hl_size))
     
     model_params = [
         C, W1, b1, W2, b2,
-        batchnorm_gain, batchnorm_bias
+        bn_gain, bn_bias
         ]
-    model_hyperparams = [hl_size, lookup_size, context_length]
+    
+    model_hyperparams = [
+        hl_size, 
+        lookup_size, 
+        context_length
+        ]
+    
+    model_batchnorm = [  # running estimation is calculated during training -- to be used in non-batch inference
+        bn_mean_running,
+        bn_std_running
+        ]
     
     for p in model_params:
         p.requires_grad = True 
     
-    return (model_params, model_hyperparams)
+    return (model_params, model_hyperparams, model_batchnorm)
 
 
 def init_uniform_model(hl_size: int = 200, lookup_size: int = 10, context_length: int = 3):
@@ -98,9 +110,10 @@ def init_uniform_model(hl_size: int = 200, lookup_size: int = 10, context_length
 def train_model(data: tuple, model, steps: int = 1, batch_size: int = 32):
     x, y = data
     
-    model_params, model_hyperparams = model
-    C, W1, b1, W2, b2, batchnorm_gain, batchnorm_bias = model_params
+    model_params, model_hyperparams, model_batchnorm = model
+    C, W1, b1, W2, b2, bn_gain, bn_bias = model_params
     hl_size, lookup_size, context_length = model_hyperparams
+    bn_mean_running, bn_std_running = model_batchnorm
     
     for i in range(steps):
         # get random batch
@@ -109,7 +122,18 @@ def train_model(data: tuple, model, steps: int = 1, batch_size: int = 32):
         # forward pass
         emb = C[x[ix]]
         h_preact = emb.view(-1, lookup_size*context_length) @ W1 + b1
-        h_preact = batchnorm_gain * (h_preact - h_preact.mean(0, keepdim=True)) / h_preact.std(0, keepdim=True) + batchnorm_bias
+        
+        bn_meani = h_preact.mean(0, keepdim=True)
+        bn_stdi = h_preact.std(0, keepdim=True)
+        h_preact = bn_gain * (h_preact - bn_meani) / (bn_stdi + 1e-5) + bn_bias
+        # BatchNorm side effect: regularizes the input (augments the data, reduces overfitting)
+        #   It forms relationships between different batches
+        
+        # store running mean and std for non-batch predictions i.e. in generate_samples()
+        with torch.no_grad(): # exclude from grad descent
+            bn_mean_running = 0.999 * bn_mean_running + 0.001 * bn_meani
+            bn_std_running = 0.999 * bn_std_running + 0.001 * bn_stdi
+        
         h = torch.tanh(h_preact)
         logits = h @ W2 + b2
         loss = F.cross_entropy(logits, y[ix])
@@ -124,9 +148,14 @@ def train_model(data: tuple, model, steps: int = 1, batch_size: int = 32):
         for p in model_params:
             p.data += -lr * p.grad
             
+        nll = loss.log10().item()
+            
         # learning stats
         stepi.append(i)
-        lossi.append(loss.log10().item()) # neg log-
+        lossi.append(nll) # neg log-likelihood
+        
+        if i % 500 == 0: 
+            print(f"Epoch: {i}, Loss: {nll}")
         
     print(f"{loss=}")
     plt.plot(stepi, lossi)
@@ -135,8 +164,9 @@ def train_model(data: tuple, model, steps: int = 1, batch_size: int = 32):
 def generate_samples(model, num_samples: int = 10, block_size: int = 3):
     g = torch.Generator().manual_seed(SEED)
     
-    model_params, _ = model
-    C, W1, b1, W2, b2, batchnorm_gain, batchnorm_bias = model_params
+    model_params, _, model_batchnorm = model
+    C, W1, b1, W2, b2, bn_gain, bn_bias = model_params
+    bn_mean_running, bn_std_running = model_batchnorm
     
     for _ in range(num_samples):
         out = []
@@ -146,7 +176,7 @@ def generate_samples(model, num_samples: int = 10, block_size: int = 3):
             emb = C[torch.tensor([context])] # [1, block_size, d]
             
             h_preact = emb.view(1, -1) @ W1 + b1
-            h_preact = batchnorm_gain * (h_preact - h_preact.mean(0, keepdim=True)) / h_preact.std(0, keepdim=True) + batchnorm_bias
+            h_preact = bn_gain * (h_preact - bn_mean_running) / bn_std_running + bn_bias
             h = torch.tanh(h_preact)
             logits = h @ W2 + b2
             probs = F.softmax(logits, dim=1)
@@ -165,16 +195,17 @@ def generate_samples(model, num_samples: int = 10, block_size: int = 3):
 @torch.no_grad()
 def validate_model(model, data: tuple):
     
-    model_params, model_hyperparams = model
-    C, W1, b1, W2, b2, batchnorm_gain, batchnorm_bias = model_params
+    model_params, model_hyperparams, model_batchnorm = model
+    C, W1, b1, W2, b2, bn_gain, bn_bias = model_params
     hl_size, lookup_size, context_length = model_hyperparams
-    
+    bn_mean_running, bn_std_running = model_batchnorm
+
     x, y = data
 
     emb = C[x]
     
     h_preact = emb.view(-1, lookup_size*context_length) @ W1 + b1
-    h_preact = batchnorm_gain * (h_preact - h_preact.mean(0, keepdim=True)) / h_preact.std(0, keepdim=True) + batchnorm_bias
+    h_preact = bn_gain * (h_preact - bn_mean_running) / bn_std_running + bn_bias
     h = torch.tanh(h_preact)
     logits = h @ W2 + b2
     loss = F.cross_entropy(logits, y)
@@ -198,7 +229,7 @@ stepi = []
 train_model(
     data=(xtr, ytr),
     model=model, 
-    steps=15000,
+    steps=30000,
     batch_size=64
     )
 # %%
